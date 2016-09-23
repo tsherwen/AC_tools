7094b61eaabcaddc2be08677792a72ccafebc0bd
Ben Newsome <bn506@york.ac.uk>
2016-09-23 18:34:32 +0100

    Created setup.py with a dependency - possibly needs expanding.

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(  name='AC_tools',
+        version='0.1',
+        description='Atmospheric Chemistry tools',
+        author='Tomas Shewen, Ben Newsome',
+        author_email='Tomas.Sherwn@york.ac.uk',
+        url='https://github.com/tsherwen/AC_tools',
+        licence="why are they all so complicated - citatios would be nice :-)"
+        packages['AC_tools'],
+        install_requires=[
+            'numpy',
+        ],
+        )
+        
