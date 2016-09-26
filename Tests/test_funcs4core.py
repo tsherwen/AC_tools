e9e9b8a672fe0ddf4a7c37fdc311cb7686308695
Ben Newsome <bn506@york.ac.uk>
2016-09-26 14:37:06 +0100

    Added test for gchemgrid.

--- Tests/test_funcs4core.py
+++ Tests/test_funcs4core.py
@@ -1,8 +1,7 @@
-from ..bpch2netCDF import *
+from ..funcs4core import *
 import logging
 import pytest
 logging.basicConfig(filename='test.log',level=logging.DEBUG)                    
-logging.info('Starting funcs4GEOSC test.') 
 
 noCTM = pytest.mark.skipif(                                                     
     not pytest.config.getoption("--remake_ctm"),                                
@@ -13,11 +12,20 @@ noCTM = pytest.mark.skipif(
 def test_convert_to_netCDF():
     return
 
-def test_get_folder():
+def test_gchemgrid():
+    # Test fail for no inputs
+    with pytest.raises(Exception):
+        gchemgrid()
+
+    # Test dictionary return
+    arr = gchemgrid('c_km_geos5_r')
+    assert isinstance( arr, np.ndarray), 'item is not a numpy array'
+    
+    dic = gchemgrid( rtn_dict=True )
+    assert isinstance( dic, dict), 'Dictionary return failed.'
     return
+    
 
 
 
 
-
-logging.info('funcs4GEOSC test complete')
