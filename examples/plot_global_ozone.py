a9d696921f804af139bb6c9dcc4b299de7d4a2c1
Ben Newsome <bn506@york.ac.uk>
2016-10-24 15:23:40 +0100

    Added saveplot and showplot to simple example

--- examples/plot_global_ozone.py
+++ examples/plot_global_ozone.py
@@ -1,41 +1,19 @@
-
-from __future__ import absolute_import
 import AC_tools as AC
-import sys
-sys.path=["/work/home/bn506/Python"]+sys.path
-print sys.path
 
-import pygchem
+# Specify the working directory
+wd = "example_data"
 
+# Get the GeosChem species data from the wd
+my_data = AC.get_GC_output( wd, species='O3')
 
+# Get a 2d slice from the 3d array
+O3_data = O3_data[:,:,0]
 
+# Create the plot
+AC.map_plot( O3_data)
 
-def main():
-
-    # Specify the directory of the output data
-    results_dir = "/work/home/bn506/temp/temp_shani_res"
-    wd = results_dir
-
-    # Get the GeosChem output
-    #O3_data = AC.get_GC_output( results_dir, species='O3')
-#    O3_data = AC.get_GC_output( results_dir, vars=['BIOGSRCE__ISOP'])
-    O3_data = AC.get_GC_output( results_dir, vars=['vov'])
-
-
-    # Get a 2d slice from the 3d array
-    # Data is in the form lat,lon,alt
-    O3_data = O3_data[:,:,0]
-
-    print O3_data.shape
-    O3_data = O3_data.T
-
-    AC.map_plot( O3_data , res='0.5x0.625', wd=wd)
-#    AC.map_plot( O3_data , wd=wd)
-
-    import matplotlib.pyplot as plt
-    plt.show()
-
-if __name__ == '__main__':
-    main()
+# Save the plot and show it.
+AC.save_plot("my_plot")
+AC.show_plot()
 
 
