9625758e26bf481e7f6148ba731e1b0b263403d8
Ben Newsome <bn506@york.ac.uk>
2016-10-24 16:23:29 +0100

    Cleaned up and simplified + addded data downlaoder

--- examples/plot_global_ozone.py
+++ examples/plot_global_ozone.py
@@ -1,5 +1,13 @@
 import AC_tools as AC
 
+# Download the example data if it is not already downloaded.
+from AC_tools.Scripts import get_example_files
+
+# Add some logging
+import logging
+#logging.basicConfig(filename='example.log',level=0)     
+
+
 # Specify the working directory
 wd = "example_data"
 
@@ -7,10 +15,10 @@ wd = "example_data"
 my_data = AC.get_GC_output( wd, species='O3')
 
 # Get a 2d slice from the 3d array
-O3_data = O3_data[:,:,0]
+my_data = my_data[:,:,0]
 
 # Create the plot
-AC.map_plot( O3_data)
+AC.map_plot( my_data)
 
 # Save the plot and show it.
 AC.save_plot("my_plot")
