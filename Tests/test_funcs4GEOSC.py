2ce22a6964be6f703eed41c23a87efcaa9c32a83
Ben Newsome <bn506@york.ac.uk>
2016-10-26 17:13:08 +0100

    Updated tests

--- Tests/test_funcs4GEOSC.py
+++ Tests/test_funcs4GEOSC.py
@@ -1,27 +1,32 @@
-#from ..funcs4GEOSC import *
-from AC_tools import *
+from ..funcs4GEOSC import *
 import logging
 import pytest
 
-wd = 'test_files'
-
+wd = '../data'
 
 def test_get_surface_area():
     arr = get_surface_area(wd=wd)
     assert isinstance( arr, np.ndarray), 'Surface area not a numpy array.'
     assert (len(arr.shape)==3), 'Surface area does not have 2 dimensions.'
-    # Test without passing a wd. Requires a data source.
+
+
+    # TO-DO
+    # Test without passing a wd. Requires a data source. current implementation of this is a bit cludgey.
 #    arr = get_surface_area()
+#    assert isinstance( arr, np.ndarray), 'Surface area not a numpy array.'
+#    assert (len(arr.shape)==3), 'Surface area does not have 2 dimensions.'
+    
     # Warning, this only works for tomas as requires setting up stuff.
     # The setup should not be needed if wd is passed about.
 
     return
 
-#def test_get_land_map():
-#    arr = get_land_map(wd=wd)
-#    assert isinstance( arr, np.ndarray), 'Land map not a numpy array.'
-#    assert (len(arr.shape) == 3), 'Land map has too many dimensions.'
-#    return
+def test_get_land_map():
+    # Test specifying wd
+    arr = get_land_map(wd=wd)
+    assert isinstance( arr, np.ndarray), 'Land map not a numpy array.'
+    assert (len(arr.shape) == 3), 'Land map has too many dimensions.'
+    return
 
 def test_get_O3_burden():
     var = get_O3_burden(wd=wd)
