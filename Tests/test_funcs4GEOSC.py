16eafa670a163927da5be6b44c30b5350455ed91
Ben Newsome <bn506@york.ac.uk>
2016-10-26 15:40:30 +0100

    Added more tests

--- Tests/test_funcs4GEOSC.py
+++ Tests/test_funcs4GEOSC.py
@@ -17,16 +17,18 @@ def test_get_surface_area():
 
     return
 
-def get_land_map():
-    arr = get_land_map(wd=wd)
-    assert isinstance( arr, np.ndarray), 'Land map not a numpy array.'
-    assert (len(arr.shape) == 3), 'Land map has too many dimensions.'
+#def test_get_land_map():
+#    arr = get_land_map(wd=wd)
+#    assert isinstance( arr, np.ndarray), 'Land map not a numpy array.'
+#    assert (len(arr.shape) == 3), 'Land map has too many dimensions.'
+#    return
+
+def test_get_O3_burden():
+    var = get_O3_burden(wd=wd)
+    assert (var == 10), "The O3 burden is wrong ({var})".format(var=var)
     return
 
-
-
 def test_get_air_mass_np():
-#    from funcs4GEOSC import get_air_mass_np
     arr = get_air_mass_np(wd=wd)
     assert isinstance( arr, np.ndarray), 'Air mass array is not a numpy array'
     return
@@ -34,5 +36,6 @@ def test_get_air_mass_np():
 def test_get_GC_output():
     arr = get_GC_output(wd=wd, species='O3', category='IJ_AVG_S')
     assert isinstance( arr, np.ndarray), 'GC output is not a numpy array'
+    assert round(arr.sum(),6)==round(0.14242639,6), "The ozone budget doesnt seem correct({bud})".format(bud=arr.sum())
     return
 
