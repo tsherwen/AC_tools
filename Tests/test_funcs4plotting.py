d31af17891638f5328bce7b460fd1dc5bf1f7e09
Ben Newsome <bn506@york.ac.uk>
2016-11-02 19:53:58 +0000

    Updated test_data to AC_tools/data

--- Tests/test_funcs4plotting.py
+++ Tests/test_funcs4plotting.py
@@ -1,18 +1,27 @@
 from ..funcs4plotting import *
 import logging
 import pytest
+import os
 logging.basicConfig(filename='test.log',level=logging.DEBUG)
+
+
+wd='../data'
+out_dir = 'test_output'
 
 slow = pytest.mark.skipif(                                                      
     not pytest.config.getoption("--slow"),                                      
     reason="need --slow option to run"                                          
 )                                                                               
-        
-wd='test_files'
+ 
+if not os.path.exists(out_dir):
+    os.mkdir(out_dir)
+
+
+
 @pytest.fixture()
 def test_data():
     from ..funcs4GEOSC import get_GC_output 
-    test_data = get_GC_output('test_files', species='O3') 
+    test_data = get_GC_output(wd, species='O3') 
     return test_data
 
 @slow
@@ -27,6 +36,9 @@ def test_map_plot(test_data):
 
 @slow
 def test_save_plot(test_data):
+    # Change to the test_output dir)
+
+    # Try the multiple plots
     map_plot(test_data[:,:,0])
 
     save_plot()
@@ -35,15 +47,15 @@ def test_save_plot(test_data):
     save_plot(title="test_1")
     filename_1 = "test_1.png"
 
-    save_plot(title="test_2", location=wd)
-    filename_2 = os.path.join(wd, "test_2.png")
+    save_plot(title="test_2", location=out_dir)
+    filename_2 = os.path.join(out_dir, "test_2.png")
 
     save_plot(title="test_3", location="new_folder")
     filename_3 = os.path.join("new_folder", "test_3.png")
 
-    save_plot(title="test_4", location=wd, extensions=["pdf", "png"])
-    filename_4 = os.path.join(wd, "test_4.png")
-    filename_5 = os.path.join(wd, "test_4.pdf")
+    save_plot(title="test_4", location=out_dir, extensions=["pdf", "png"])
+    filename_4 = os.path.join(out_dir, "test_4.png")
+    filename_5 = os.path.join(out_dir, "test_4.pdf")
 
     # Test plot has been created, and then remove it"
     filenames = [filename_0, filename_1, filename_2, 
@@ -58,6 +70,6 @@ def test_save_plot(test_data):
     
 
 
-
+pytest.main()
 
 
