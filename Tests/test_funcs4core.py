9c47583818f7cdcc86bc7ef856f56917d78e6a80
Ben Newsome <bn506@york.ac.uk>
2016-11-02 20:56:27 +0000

    added get_gc_res tests

--- Tests/test_funcs4core.py
+++ Tests/test_funcs4core.py
@@ -15,6 +15,14 @@ def test_gchemgrid():
     dic = gchemgrid( rtn_dict=True )
     assert isinstance( dic, dict), 'Dictionary return failed.'
     return
+
+def test_get_dims4res():
+    # test the dictionary returns
+    test_dict = get_dims4res( r_dims=True )
+    assert (test_dict[72,46,47] == '4x5'), '4x5 res lookup failed'
+
+    test_dict_2d = get_dims4res( r_dims=True, just2D=True)
+    assert (test_dict_2d[72,46] == '4x5'), '4x5 res lookup failed'
     
 
 
