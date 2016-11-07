6879b7ce3c8fa3f34d8982ccf34e55bc63cf17da
Ben Newsome <bn506@york.ac.uk>
2016-11-07 19:04:24 +0000

    Added unit conversion to the example

--- examples/plot_global_ozone.py
+++ examples/plot_global_ozone.py
@@ -9,8 +9,12 @@ wd = "../data"
 # Get the GeosChem species data from the wd
 my_data = AC.get_GC_output( wd, species='O3')
 
+
 # Get a 2d slice from the 3d array
 my_data = my_data[:,:,0,0]
+
+# Turn from part per part to part per billion
+my_data = my_data*1E9
 
 # Create the plot
 AC.map_plot( my_data)
