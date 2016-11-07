e656165a61488d5bcaca47b76472051bf20a849a
Ben Newsome <bn506@york.ac.uk>
2016-11-07 14:08:15 +0000

    Added tests for get_latlonalt4res()

--- Tests/test_funcs4core.py
+++ Tests/test_funcs4core.py
@@ -1,7 +1,10 @@
 from ..funcs4core import *
 import logging
 import pytest
+import numpy as np
 logging.basicConfig(filename='test.log',level=logging.DEBUG)                    
+
+data_dir = '../data'
 
 def test_gchemgrid():
     # Test fail for no inputs
@@ -23,8 +26,30 @@ def test_get_dims4res():
 
     test_dict_2d = get_dims4res( r_dims=True, just2D=True)
     assert (test_dict_2d[72,46] == '4x5'), '4x5 res lookup failed'
-    
 
+
+def test_get_latlonalt4res_default():
+
+    # Test default
+    lon, lat, alt = get_latlonalt4res()
+    print type(lon)
+    assert isinstance(lon, np.ndarray), 'The lon is not correct.'
+    assert isinstance(lat, np.ndarray), 'The lat is not correct.'
+    assert isinstance(alt, np.ndarray), 'The alt is not correct.'
+    
+def test_get_latlonalt4res_res():
+    lon, lat, alt = get_latlonalt4res(res='4x5')
+    assert isinstance(lon, np.ndarray), 'The lon is not correct.'
+    assert isinstance(lat, np.ndarray), 'The lat is not correct.'
+    assert isinstance(alt, np.ndarray), 'The alt is not correct.'
+    # Test give_resolution
+
+def test_get_latlonalt4res_wd():
+    lon, lat, alt = get_latlonalt4res(wd=data_dir)
+    assert isinstance(lon, np.ndarray), 'The lon is not correct.'
+    assert isinstance(lat, np.ndarray), 'The lat is not correct.'
+    assert isinstance(alt, np.ndarray), 'The alt is not correct.'
+    # Test give wd
 
 
 
