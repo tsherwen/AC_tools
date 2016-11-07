04a709cfb38ce75ad93a4017844ab6e4b03a561b
Ben Newsome <bn506@york.ac.uk>
2016-11-07 18:15:52 +0000

    Added get_sigfig tests, updated dim4res tests, updated latlonalt4res tests

--- Tests/test_funcs4core.py
+++ Tests/test_funcs4core.py
@@ -6,6 +6,24 @@ logging.basicConfig(filename='test.log',level=logging.DEBUG)
 
 data_dir = '../data'
 
+def test_get_sigfig():                                                          
+    print get_sigfig(100,1)
+    assert(get_sigfig(3.22294, 1)==3)                                           
+    assert(get_sigfig(3.29294, 2)==3.3)                                         
+    assert(get_sigfig(3.29294, 3)==3.29)                                        
+    return                                                                      
+                                                                                
+def test_get_sigfig_big():                                                      
+    assert(get_sigfig(3.22294E20, 1)==3E20)                                     
+    assert(get_sigfig(3.29294E20, 2)==3.3E20)                                   
+    assert(get_sigfig(3.29294E20, 3)==3.29E20)                                  
+                                                                               
+def test_get_sigfig_small():                                                    
+    assert(get_sigfig(3.22294E-20, 1)==3E-20)                                   
+    assert(get_sigfig(3.29294E-20, 2)==3.3E-20)                                 
+    assert(get_sigfig(3.29294E-20, 3)==3.29E-20)                                
+    return                                                                      
+
 def test_gchemgrid():
     # Test fail for no inputs
     with pytest.raises(Exception):
@@ -19,37 +37,35 @@ def test_gchemgrid():
     assert isinstance( dic, dict), 'Dictionary return failed.'
     return
 
-def test_get_dims4res():
+def test_get_dims4res_4x5():
     # test the dictionary returns
-    test_dict = get_dims4res( r_dims=True )
-    assert (test_dict[72,46,47] == '4x5'), '4x5 res lookup failed'
+    res = get_dims4res('4x5')
+    assert (res == (72,46,47)), '4x5 res lookup failed'
 
-    test_dict_2d = get_dims4res( r_dims=True, just2D=True)
-    assert (test_dict_2d[72,46] == '4x5'), '4x5 res lookup failed'
+def test_get_dims4res_2x25():
+    # test the dictionary returns
+    res = get_dims4res( '2x2.5' )
+    assert (res == (144,91,47)), '2x2.5 res lookup failed'
+
+
+def test_get_dims4res_2D():
+    res_2D = get_dims4res( '4x5', just2D=True)
+    assert (res_2D == (72, 46)), "2D lookup failed"
+
+
+def test_get_dims4res_dict():
+    test_dict_2d = get_dims4res( r_dims=True)
+    assert (test_dict_2d[72,46,47] == '4x5'), 'dict lookup failed'
 
 
 def test_get_latlonalt4res_default():
 
     # Test default
-    lon, lat, alt = get_latlonalt4res()
-    print type(lon)
-    assert isinstance(lon, np.ndarray), 'The lon is not correct.'
-    assert isinstance(lat, np.ndarray), 'The lat is not correct.'
-    assert isinstance(alt, np.ndarray), 'The alt is not correct.'
-    
-def test_get_latlonalt4res_res():
-    lon, lat, alt = get_latlonalt4res(res='4x5')
-    assert isinstance(lon, np.ndarray), 'The lon is not correct.'
-    assert isinstance(lat, np.ndarray), 'The lat is not correct.'
-    assert isinstance(alt, np.ndarray), 'The alt is not correct.'
-    # Test give_resolution
-
-def test_get_latlonalt4res_wd():
-    lon, lat, alt = get_latlonalt4res(wd=data_dir)
-    assert isinstance(lon, np.ndarray), 'The lon is not correct.'
-    assert isinstance(lat, np.ndarray), 'The lat is not correct.'
-    assert isinstance(alt, np.ndarray), 'The alt is not correct.'
-    # Test give wd
+    (lon, lat, alt) = get_latlonalt4res()
+    print "lon = ", lon
+    assert len(lat)==46, 'The default latitude is wrong'
+    assert len(lon)==72, 'The default longitude is wrong'
+    assert len(alt)==47, 'The default altidure is wrong'
 
 
 
