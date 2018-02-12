bf853b7e489a1e645980fccc00d206383e9f1ca2
Tomas Sherwen <tomas.sherwen@york.ac.uk>
2018-02-12 17:19:31 +0000

    Include future print in init + tidy logging vars

--- __init__.py
+++ __init__.py
@@ -4,12 +4,16 @@ To access the help, from python or ipython, type help(AC_tools) to get general h
 To get more detailed help from a module for example, type help(AC_tools.funcs4time.py)
 If you find missing documentation any thing is unclear in any of this, please request a git push to github.
 """
+# compatibility with both python 2 and 3
+from __future__ import print_function
 
+# Setup logging for module
 import logging
+level=logging.DEBUG
 FORMAT = "%(levelname)8s - %(message)s   @---> %(filename)s:%(lineno)s  %(funcName)s()"
-logging.basicConfig(filename='AC_tools.log', filemode='w',level=logging.DEBUG,
+logging.basicConfig(filename='AC_tools.log', filemode='w',level=level,
                 format=FORMAT)
-logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger().setLevel(level)
 
 import numpy as np
 # Import submodules here for easier access
