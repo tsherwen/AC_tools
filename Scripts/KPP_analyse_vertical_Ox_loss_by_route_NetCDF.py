c1454faf6a7917fc37bb9d834ec67dd25732bff5
Tomas Sherwen <tomas.sherwen@york.ac.uk>
2020-08-11 18:23:41 +0100

    interim commit of pseudo code setup for NetCDF KPP

--- Scripts/KPP_analyse_vertical_Ox_loss_by_route_NetCDF.py
+++ Scripts/KPP_analyse_vertical_Ox_loss_by_route_NetCDF.py
@@ -17,7 +17,7 @@ from netCDF4 import Dataset
 import os
 
 
-def main(wd=None, CODE_wd=None):
+def main(wd=None, CODE_wd=None, verbose=False, debug=False):
     """
     Driver for Ox loss analysis via KPP in GEOS-Chem from NetCDF output
 
@@ -25,27 +25,76 @@ def main(wd=None, CODE_wd=None):
     -----
      - comment/uncomment functions as required
     """
+    # - Local variables
+    fam = 'LOx'
+    ref_spec = 'O3'
     # Manually set locations of model output with the Ox loss diagnostic
     root = '/users/ts551/scratch/GC/'
-    CODE_wd = root+'/Code/Code.v11-02_Cl_v3_0/'
+    CODE_wd = root + '/Code/Code.BleedingEdge/'
     wd = root + '/rundirs/'
     wd += 'merra2_4x5_standard.v12.9.1.BASE.Oi.MacDonald2014.tagged/'
 #    Mechanism = 'Tropchem'
     Mechanism = 'Standard'
+    # Get locations of model output/core
+    assert os.path.exists(wd), 'working directory not found @: {}'.format(wd)
+    CODE_wd = '/{}/KPP/{}/'.format(CODE_wd, Mechanism)
+    assert os.path.exists(CODE_wd), 'code directory not found @: ' + CODE_wd
+
+    # - Model output from tagged mechanism run
+
+    # Get all the necessary data as as a dictionary object?
+    # NOTE: this is a previous approach using dictionaries
+
     # Get a dictionary of information about the model run
     # NOTE: Should this approach be retired? Yes.
     full_vert_grid = True
     VarDict = AC.get_default_variable_dict(full_vert_grid=full_vert_grid,
                                            wd=wd)
-    # Now Get the StateMet object...
+
+    # Get the prod/loss netCDFs
+    dsPL = get_ProdLoss_ds(wd=wd)
+
+    # Now Get the StateMet object... for time in troposphere diagnostic
 
 
+    # - KPP mechanism
+    # Get the dictionary of the KPP mechanism.
+    Ox_fam_dict = AC.get_Ox_fam_dicts(fam=fam, ref_spec=ref_spec,
+                                      Mechanism=Mechanism,
+                                      wd=wd, CODE_wd=CODE_wd,
+                                      )
 
-    # Get all the necessary data as as a dictionary object
-    Ox_loss_dict = AC.get_Ox_loss_dicts(
-        wd=wd, CODE_wd=CODE_wd, Mechanism=Mechanism)
+
+    # - Now process the tagged model output using the info extracted from KPP
+    #
+    # Convert the units from molec/cm3/s to Gg/s
+
+    # Update for stoichiometry of the reaction routes
+
+    # If requested, remove the troposphere
+
+    # If monthly output, scale to once per month?
+    # For now, just use the average over time?
+    # NO. (as later functions require by month)
+
+    #
+    ars = get_PL_ars4mech_NetCDF(wd=wd,
+                                )
+
+    # Add this to a common KPP/model dictionary and pass this to a function to be analysed
+    Ox_loss_dict['ars'] = ars
+
+    # - Now plot up/do analysis
+
     # Plot vertical odd oxygen (Ox) loss via route (chemical family)
-    plot_vertical_fam_loss_by_route(
-        Ox_loss_dict=Ox_loss_dict, Mechanism=Mechanism)
+    plot_vertical_fam_loss_by_route(Ox_loss_dict=Ox_loss_dict,
+                                    Mechanism=Mechanism)
     # Analyse odd oxygen (Ox) loss budget via route (chemical family)
     calc_fam_loss_by_route(Ox_loss_dict=Ox_loss_dict, Mechanism=Mechanism)
+
+
+
+
+
+
+
